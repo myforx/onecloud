80b21d0e4424a1a0054b8fe89375ea87259279b0
Ilya Brin <wimnorder@users.noreply.github.com>
2017-11-02 01:13:27 +0300

    New DNS methods added

--- lib/onecloud/dns.rb
+++ lib/onecloud/dns.rb
@@ -1,14 +1,83 @@
 module Onecloud
   # Operations with DNS
   module DNS # TODO: Rename to 'Domains'
-    # List of all domains
-    def domains
-      get("Dns") # TODO: alias: all_dns, dns, dns_list
+    # Get record by ID
+    def dns_by_id(record_id)
+      get("dns/record/#{record_id}")
     end
 
-    # Domain by ID
     def domain_by_id(domain_id)
-      get("Dns/#{domain_id}")
+      get("dns/#{domain_id}")
+    end
+
+    # Update CNAME record
+    def dns_update_cname(record_id, params)
+      put("dns/recordname/#{record_id}", params)
+    end
+
+    # Update MX record
+    def dns_update_mx(record_id, params)
+      put("dns/recordmx/#{record_id}", params)
+    end
+
+    # Update NS record
+    def dns_update_ns(record_id, params)
+      put("dns/recordns/#{record_id}", params)
+    end
+
+    # Update SRV record
+    def dns_update_srv(record_id, params)
+      put("dns/recordsrc/#{record_id}", params)
+    end
+
+    # Update TXT record
+    def dns_update_txt(record_id, params)
+      put("dns/recordtxt/#{record_id}", params)
+    end
+
+    # Update A record
+    def dns_update_a(record_id, params)
+      put("dns/recorda/#{record_id}", params)
+    end
+
+    # Update AAAA record
+    def dns_update_aaaa(record_id, params)
+      put("dns/recordaaaa/#{record_id}", params)
+    end
+
+    # Create CNAME record
+    def dns_create_cname(params)
+      post('dns/recordcname', params)
+    end
+
+    # Create MX record
+    def dns_create_mx(params)
+      post('dns/recordmx', params)
+    end
+
+    # Create NS record
+    def dns_create_ns(params)
+      post('dns/recordns', params)
+    end
+
+    # Create SRV record
+    def dns_create_srv(params)
+      post('dns/recordsrc', params)
+    end
+
+    # Create TXT record
+    def dns_create_txt(params)
+      post('dns/recordtxt', params)
+    end
+
+    # Create A record
+    def dns_create_a(params)
+      post('dns/recorda', params)
+    end
+
+    # Create AAAA record
+    def dns_create_aaaa(params)
+      post('dns/recordaaaa', params)
     end
 
     # Create new domain
@@ -16,49 +85,19 @@ module Onecloud
       post('dns', params)
     end
 
+    # List of all domains
+    def domains
+      get('dns') # TODO: alias: all_dns, dns, dns_list
+    end
+
     # Delete domain by ID
     def remove_domain(domain_id)
       delete("dns/#{domain_id}")
     end
 
-    # Create A type record
-    def add_domain_record_A(params)
-      post('dns/recorda', params)
-    end
-
-    # Create AAAA type record
-    def add_domain_record_AAAA(params)
-      post('dns/recordaaaa', params)
-    end
-
-    # Create CNAME type record
-    def add_domain_record_CNAME(params)
-      post('dns/recordcname', params)
-    end
-
-    # Create MX type record
-    def add_domain_record_MX(params)
-      post('dns/recordmx', params)
-    end
-
-    # Create NS type record
-    def add_domain_record_NS(params)
-      post('dns/recordns', params)
-    end
-
-    # Create TXT type record
-    def add_domain_record_TXT(params)
-      post('dns/recordtxt', params)
-    end
-
-    # Remove record for domain by his ID
+    # Remove record for domain by ID
     def remove_domain_record(domain_id, record_id)
       delete("dns/#{domain_id}/#{record_id}")
-    end
-
-    # Create SRV type record
-    def add_domain_record_SRV(params)
-      post('dns/recordsrv', params)
     end
   end
 end
