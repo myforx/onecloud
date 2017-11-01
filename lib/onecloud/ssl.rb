74b20f472316bf36e8318d088fcb677044d1e986
Ilya Brin <wimnorder@users.noreply.github.com>
2017-11-02 01:15:53 +0300

    SSL module added

--- lib/onecloud/ssl.rb
+++ lib/onecloud/ssl.rb
@@ -6,19 +6,19 @@ module Onecloud
     # - SanDomains list - List of domains for adding
     # - CreateCnameRecord bool - (optional) Create or not CNAME-record
     #   If domain delegated to 1cloud DNS and validation method is DNS
-    def add_domain(domain_id, params)
-      put("ssl/{domain_id}/addsan", params)
+    def ssl_add_domain_to_cert(domain_id, params)
+      put("ssl/#{domain_id}/addsan", params)
     end
 
     # Load SSL key content
     # key_id - Integer
     # type - String { ca | crt | csr | private-key | pfx }
-    def get(cert_id, type)
-      get("/ssl/{cert_id}/file/{type}")
+    def ssl_files_by_cert_id(cert_id, type)
+      get("ssl/#{cert_id}/file/#{type}")
     end
 
     # Ordering a new certificate
-    def order_new(params)
+    def ssl_new(params)
       post('ssl', params)
     end
 
@@ -27,13 +27,13 @@ module Onecloud
     # - Enabled bool - or false
     #     true  - automatic renewal is on
     #     false - automatic renewal is off
-    def change_renewal_status(cert_id, params)
-      put("ssl/{cert_id}")
+    def ssl_change_renewal_status(cert_id, params)
+      put("ssl/#{cert_id}", params)
     end
 
     # Available cert?
-    def product_info(cert_id)
-      get("ssl/products/{cert_id}")
+    def ssl_product_info(cert_id)
+      get("ssl/products/#{cert_id}")
     end
 
     # Operations with exists cert
@@ -43,33 +43,33 @@ module Onecloud
     #     "resumeorder"
     #     "revalidate"  - retry validation procedure
     #     "cancel"      - cancel order
-    def action_with(cert_id, params)
-      post("ssl/{cert_id}/action")
+    def ssl_action_with_cert(cert_id, params)
+      post("ssl/#{cert_id}/action", params)
     end
 
     # Get cert validation status
-    def validation_status(cert_id)
-      get("ssl/{cert_id}/checkdcv")
+    def ssl_cert_validation_status(cert_id)
+      get("ssl/#{cert_id}/checkdcv")
     end
 
     # Cert info
-    def info(cert_id)
-      get("ssl/{cert_id}")
+    def ssl_cert_by_id(cert_id)
+      get("ssl/#{cert_id}")
     end
 
     # List all certificates
-    def list
+    def ssl_list
       get('ssl')
     end
 
     # Certs available for ordering
-    def available_for_ordering()
+    def ssl_certs_available_for_ordering
       get('ssl/products')
     end
 
     # Remove certificate by his ID
-    def remove(cert_id)
-      delete("ssl/{cert_id}")
+    def ssl_remove_cert(cert_id)
+      delete("ssl/#{cert_id}")
     end
   end
 end
